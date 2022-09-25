f17c0df82c263501ab67cb5be010562b2177c592
neau <davidneau49@gmail.com>
2022-09-25 19:25:33 +0100

    Add Calendar to home

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -3,15 +3,15 @@
         <v-navigation-drawer app v-model="drawer" dark style="border: solid 2px">
             <v-list>
                 <v-list-item link to='/'>
-                    <v-list-item-content>Home</v-list-item-content>
+                    Home
                 </v-list-item>
                 <v-list-item link to='/about'>
-                    <v-list-item-content>About</v-list-item-content>
+                    About
                 </v-list-item>    
             </v-list>
         </v-navigation-drawer>
 
-        <v-app-bar app color="blue darken-4" dark style="border: solid 2px">
+        <v-app-bar app color="blue darken-4" dark>
             <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
         </v-app-bar>    
     </span>
